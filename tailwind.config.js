264bb3ad4517e4b9937aad0e1b0821389bf1f6eb
Peter Pham <pttp15122002@gmail.com>
2025-02-11 07:53:58 +0700

    style(tailwindcss): add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,89 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: ["selector"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-radial":
+          "radial-gradient(closest-side at 50% 50%, #11998E 20%, #247b7b, #38EF7D, #181C14 40%)",
+        "custom-radial-light":
+          "radial-gradient(closest-side at 50% 50%, #11998E 20%, #247b7b, #38EF7D, #FFF 40%)",
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#11998E",
+          light: "#247b7b",
+        },
+        secondary: "#38EF7D",
+        gold: "#FFDD43",
+        black: {
+          DEFAULT: "#000000",
+          main: "#181C14",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          light: "#F0F0F0",
+        },
+        gray: {
+          DEFAULT: "#4C5257",
+          light: "#dee2e6",
+          dark: "#212529",
+        },
+        pink: "#ff99c8",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backdropBlur: ["responsive"],
+    },
+  },
+  plugins: [
+    animate,
+    addVariablesForColors,
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
+};
